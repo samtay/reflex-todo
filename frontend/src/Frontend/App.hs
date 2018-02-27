9bf12a9aba8cb6904ab6b99a2a92597a673a52cd
Sam Tay <sam.chong.tay@gmail.com>
2018-02-27 00:52:50 -0500

    Add deletion ability

--- frontend/src/Frontend/App.hs
+++ frontend/src/Frontend/App.hs
@@ -3,9 +3,11 @@
 {-# LANGUAGE RecursiveDo           #-}
 module Frontend.App (app) where
 
-import           Control.Monad  (void)
-import           Data.Text      (Text)
-
+import           Data.Map      (Map)
+import qualified Data.Map      as Map
+import           Data.Map.Misc (applyMap)
+import           Data.Text     (Text)
+import qualified Data.Text     as T
 import           Reflex.Dom
 
 app :: MonadWidget t m => m ()
@@ -16,20 +18,27 @@ app = do
     text "ToDo List"
   -- List items
   divClass "ui text container" $ divClass "ui segments" $ do
-    rec items <- foldDyn snoc initItems newItem
-        divClass "ui segment" $
+    rec items <- foldDyn ($) initItems $ leftmost [ mapSnoc <$> newItem
+                                                  , applyMap <$> update
+                                                  ]
+        update <- divClass "ui segment" $
           divClass "ui big ordered relaxed divided list" $ do
-            void $ simpleList items drawItem
+            listViewWithKey items drawItem
         newItem <- drawItemInput
     return ()
   where
-    initItems = [ "Send app store links to the FP meetup"
-                , "RSVP to the FP meetup"
-                , "Shower"
-                ]
+    initItems = Map.fromList $ zip [1..]
+      [ "Send app store links to the FP meetup"
+      , "RSVP to the FP meetup"
+      , "Shower"
+      ]
 
-drawItem :: MonadWidget t m => Dynamic t Text -> m ()
-drawItem = divClass "item" . divClass "content" . dynText
+drawItem :: MonadWidget t m => k -> Dynamic t Text -> m (Event t (Maybe Text))
+drawItem _ item = divClass "item" $ do
+  (trashIcon, _) <- divClass "right floated content" $
+    elClass' "i" "icon link trash alternate" blank
+  divClass "content" $ dynText item
+  return $ Nothing <$ domEvent Click trashIcon
 
 drawItemInput :: MonadWidget t m => m (Event t Text)
 drawItemInput = divClass "ui segment fluid action input" $ do
@@ -43,8 +52,10 @@ drawItemInput = divClass "ui segment fluid action input" $ do
                             ]
   return $ ffilter (not . T.null) $ tag (current $ value ti) submit
 
-snoc :: a -> [a] -> [a]
-snoc x xs = xs ++ [x]
+mapSnoc :: a -> Map Int a -> Map Int a
+mapSnoc a xs = case Map.toDescList xs of
+                 (k, _) : _ -> Map.insert (succ k) a xs
+                 _          -> Map.singleton 1 a
 
 -- TODO
 -- 1. Use 'shake' animation for deleting an item in edit mode
